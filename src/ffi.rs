072085a4da06cea48011e84911b8016c89fd7002
Zachary Sims <zac.sims@gmail.com>
2018-06-09 21:49:56 +1000

    Adds support for initializing clamav

---
+++ src/ffi.rs
@@ -0,0 +1,65 @@
+#![allow(non_camel_case_types, dead_code)]
+
+use libc::c_uint;
+
+pub const CL_INIT_DEFAULT: u32 = 0x0;
+
+// :libclamav.so.7 as opposed to clamav as libclamav.so may not exist
+#[link(name = ":libclamav.so.7")]
+extern "C" {
+    pub fn cl_init(initOptions: c_uint) -> cl_error;
+}
+
+#[repr(C)]
+#[derive(PartialEq, Debug)]
+pub enum cl_error {
+    /* libclamav specific */
+    /* CL_CLEAN = 0, */
+    CL_SUCCESS = 0,
+    CL_VIRUS,
+    CL_ENULLARG,
+    CL_EARG,
+    CL_EMALFDB,
+    CL_ECVD,
+    CL_EVERIFY,
+    CL_EUNPACK,
+
+    /* I/O and memory errors */
+    CL_EOPEN,
+    CL_ECREAT,
+    CL_EUNLINK,
+    CL_ESTAT,
+    CL_EREAD,
+    CL_ESEEK,
+    CL_EWRITE,
+    CL_EDUP,
+    CL_EACCES,
+    CL_ETMPFILE,
+    CL_ETMPDIR,
+    CL_EMAP,
+    CL_EMEM,
+    CL_ETIMEOUT,
+
+    /* internal (not reported outside libclamav) */
+    CL_BREAK,
+    CL_EMAXREC,
+    CL_EMAXSIZE,
+    CL_EMAXFILES,
+    CL_EFORMAT,
+    CL_EPARSE,
+    CL_EBYTECODE,          /* may be reported in testmode */
+    CL_EBYTECODE_TESTFAIL, /* may be reported in testmode */
+
+    /* c4w error codes */
+    CL_ELOCK,
+    CL_EBUSY,
+    CL_ESTATE,
+
+    /* no error codes below this line please */
+    CL_ELAST_ERROR,
+}
+
+impl cl_error {
+    // CL_CLEAN has the same value as CL_SUCCESS, which Rust does not yet support
+    pub const CL_CLEAN: cl_error = cl_error::CL_SUCCESS;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+extern crate libc;
+
+mod ffi;
+
+/// An error indicating a clam failure.
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct ClamError {
+    code: u32,
+}
+
+impl ClamError {
+    pub fn new(native_err: ffi::cl_error) -> Self {
+        ClamError {
+            code: native_err as u32,
+        }
+    }
+}
+
+/// Initializes clamav
+///
+/// This must be called once per process
+pub fn initialize() -> Result<(), ClamError> {
+    unsafe {
+        let result = ffi::cl_init(ffi::CL_INIT_DEFAULT);
+        match result {
+            ffi::cl_error::CL_SUCCESS => Ok(()),
+            _ => Err(ClamError::new(result)),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn initialize_success() {
+        assert!(initialize().is_ok(), "initialize should succeed");
     }
 }
